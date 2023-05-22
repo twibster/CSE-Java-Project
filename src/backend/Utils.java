d8261f4ecc47bfa5062dc50686072bc6e65cbde9
Omar Omran <omaromran2003@gmail.com>
2023-05-22 17:38:06 +0300

    rename to Utils refactore methods to naming conventions

--- src/backend/Utils.java
+++ src/backend/Utils.java
@@ -2,16 +2,20 @@ package backend;
 
 import org.springframework.security.crypto.bcrypt.BCrypt;
 
-public class Functions {
+public class Utils {
+    public static void print(Object toPrint){
+        System.out.println(toPrint);
+    }
     public static void PrintArray(Object[] array){
         for (Object item: array){
             System.out.println(item);
         }
     }
-    public static String Hash(String toHash){
+    public static String hash(String toHash){
         return BCrypt.hashpw(toHash, BCrypt.gensalt());
     }
-    public static Boolean CheckHash(String toCompare, String storedHash){
+    public static Boolean checkHash(String toCompare, String storedHash){
         return BCrypt.checkpw(toCompare, storedHash);
     }
+
 }
